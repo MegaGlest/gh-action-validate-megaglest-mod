3f03da053502e9706b53eb4e55842e03b01e8305
Jammyjamjamman <jdsherratt3@gmail.com>
2023-07-19 23:19:57 +0100

    Try to make test work

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 set -ev
 
-exit 0
-# xvfb-run ./usr/games/megaglest --validate-techtrees=insects-mod/git_repos/Insects-World-Mod/insects_world_dev/ | sed -e/======\ Started\ Validation\ ======/\{ -e:1 -en\;b1 -e\} -ed
 if [ -z "$INPUT_DIRECTORY" ]; then
     INPUT_DIRECTORY="."
 fi
+
+xvfb-run /usr/games/megaglest --validate-techtrees=$INPUT_DIRECTORY | sed -e/======\ Started\ Validation\ ======/\{ -e:1 -en\;b1 -e\} -ed > results.txt
+
+ret=0
+grep -i error results.txt || exit 0
+# if [ INPUT_FAIL_ON_WARNING = true ] then
+exit 1
+# fi
+if [ $ret eq 1]; then
+  #if fail_on_warning = true
+  # grep check for warning
+  # if warning found, exit 1 else exit 0
+fi
+exit 0
 
 OUTPUT_DIR=/github/workspace/output
 mkdir -m 777 -p $OUTPUT_DIR
